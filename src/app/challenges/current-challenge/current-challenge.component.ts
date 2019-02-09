42348f3520e15ff343ac213b25a8df8559be1f80
Vladimir Pavković <vladimirpavk@gmail.com>
2019-02-09 18:24:11 +0100

    +

---
+++ src/app/challenges/current-challenge/current-challenge.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'ns-current-challenge',
+    templateUrl: './current-challenge.component.html',
+    moduleId: module.id,
+    styleUrls: [
+        './current-challenge.component.css'
+    ]
+})
+export class CurrentChallengeComponent{
+
+}
